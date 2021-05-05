765d0bbf54ffb22fce73d509f130a1bda3f573f2
sagor <sagor@sagors-MacBook-Pro.local>
2021-05-05 14:15:52 +0600

    add example gRPC server client communicate
    Signed-off-by: sagor <sagor@sagors-MacBook-Pro.local>

---
+++ example/helloworld/greeter_server/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"context"
+	pb "github.com/Hello-gRPC/example/helloworld/helloworld"
+    "google.golang.org/grpc"
+	"log"
+	"net"
+)
+
+const (
+	port = ":50051"
+)
+
+type server struct {
+	pb.UnimplementedGreeterServer
+}
+
+func (s *server) SayHello(ctx context.Context, in *pb.Request) (*pb.Response, error)  {
+	log.Printf("Received: %v", in.GetName())
+	return &pb.Response{Message: "Hello " + in.GetName()}, nil
+}
+
+func main() {
+	lis, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatalf("failed to listen %v", err)
+	}
+
+	s := grpc.NewServer()
+	pb.RegisterGreeterServer(s, &server{})
+
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve gRPC server %v", err)
+	}
+}
